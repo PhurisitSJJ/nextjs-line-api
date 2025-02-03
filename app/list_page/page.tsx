8dd8d011e1fbd3ee4f3b289d4d03617024413946
Phurisit <phurisit369@gmail.com>
2025-02-03 11:02:33 +0700

    55 commit

---
+++ app/list_page/page.tsx
@@ -0,0 +1,18 @@
+'use client';
+
+import { Container } from "@mui/material";
+import MemberNavbar from "@/app/utils/components/MemberNavber";
+
+const ListDataPage: React.FC = () => {
+
+    return (
+        <>
+            <MemberNavbar />
+            <Container>
+
+            </Container>
+            </>
+    );
+};
+
+export default ListDataPage;
