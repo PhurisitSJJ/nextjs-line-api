c1f34a07b0e07e6524ca4d3a8707f8f562bd7408
Phurisit <phurisit369@gmail.com>
2025-02-03 11:16:50 +0700

    56 commit

--- app/utils/components/MemberNavber.tsx
+++ app/utils/components/MemberNavber.tsx
@@ -1,9 +1,13 @@
 import liff from '@line/liff';
 import { AppBar, Toolbar, Box, Button, Typography } from '@mui/material';
 import LogoutIcon from '@mui/icons-material/Logout';
+import { useRouter } from 'next/navigation';
 import * as React from 'react';
 
+
 const MemberNavbar: React.FC = () => {
+    const router = useRouter();
+
     const handleLogout = () => {
         liff.logout();
         window.location.href = '/login_page';
@@ -14,16 +18,16 @@ const MemberNavbar: React.FC = () => {
             <Toolbar sx={{ px: 2, display: 'flex', justifyContent: 'space-between' }}>
                 
                 <Box sx={{ display: 'flex', gap: 4 }}>
-                    <Typography variant="h6" sx={{ color: 'white', cursor: 'pointer' }}>
+                    <Typography variant="h6" sx={{ color: 'white', cursor: 'pointer'}} onClick={() => router.push('/home_page')}>
                         หน้าหลัก
                     </Typography>
-                    <Typography variant="h6" sx={{ color: 'white', cursor: 'pointer' }}>
+                    <Typography variant="h6" sx={{ color: 'white', cursor: 'pointer' }} onClick={() => router.push('/search_page')}>
                         ค้นหา
                     </Typography>
-                    <Typography variant="h6" sx={{ color: 'white', cursor: 'pointer' }}>
+                    <Typography variant="h6" sx={{ color: 'white', cursor: 'pointer' }} onClick={() => router.push('/list_page')}>
                         รายการข้อมูล
                     </Typography>
-                    <Typography variant="h6" sx={{ color: 'white', cursor: 'pointer' }}>
+                    <Typography variant="h6" sx={{ color: 'white', cursor: 'pointer' }} onClick={() => router.push('/add_page')}>
                         เพิ่มข้อมูล
                     </Typography>
                 </Box>
