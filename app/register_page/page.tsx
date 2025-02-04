5b888ec0e74e7eb13c06e6932e56d7930b349347
Phurisit <phurisit369@gmail.com>
2025-02-04 13:56:41 +0700

    83 commit

--- app/register_page/page.tsx
+++ app/register_page/page.tsx
@@ -1,2 +1,18 @@
 'use client'
 
+import { Container } from "@mui/material";
+import MemberNavbar from "@/app/utils/components/MemberNavber";
+
+const RegisterPage = () => {
+
+    return (
+        <>
+            <MemberNavbar />
+            <Container>
+
+            </Container>
+            </>
+    );
+};
+
+export default RegisterPage;
